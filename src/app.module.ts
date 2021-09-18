145ed8debb13570483e7e16b565595d60da0f59b
Dmitriy Filatchev <filatchev@rg.ru>
2021-09-18 13:48:02 +0300

    feat: add mongo db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,40 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const username = configService.get('MONGO_USERNAME');
+        const password = configService.get('MONGO_PASSWORD');
+        const database = configService.get('MONGO_DATABASE');
+        const host = configService.get('MONGO_HOST');
+        const port = configService.get('MONGO_PORT');
+        const createUri = ({ username, password, host, port }) =>
+          `mongodb://${username}:${password}@${host}:${port}`;
+
+        const cp = {
+          uri: createUri({
+            username,
+            password,
+            host,
+            port,
+          }),
+          dbName: database,
+        };
+        console.log(cp);
+        return cp;
+      },
+      inject: [ConfigService],
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
